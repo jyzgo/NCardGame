0539cb8f4c4e453dc5b8fea9f81046b98a7e7bb5
ethan <ethanjyzgo@gmail.com>
2025-03-31 00:13:46 +0800

    Build0.013bb

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Playtwist-NCard-0.01b";
+const cacheName = "Playtwist-NCard-0.013b";
 const contentToCache = [
     "Build/NCardGame.loader.js",
     "Build/NCardGame.framework.js",
